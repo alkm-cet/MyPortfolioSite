a65d51d0d43b637d4549764ca526f47f55180d63
Ali Kemal Çetindere <117645598+alkm-cet@users.noreply.github.com>
2023-03-08 13:41:11 +0300

    GYMAPP

--- src/context/ProjectsContext.jsx
+++ src/context/ProjectsContext.jsx
@@ -13,24 +13,46 @@ import weatherApp from '../img/weatherApp.PNG'
 import creditCard from '../img/creditCard.PNG'
 import Planti from '../img/Planti.PNG'
 import FirebaseNotes from '../img/FirebaseNotes.PNG'
+import gymapp from '../img/gymapp.PNG'
 
 
 export const ProjectsContext = createContext();
 
 const projects = [
     {
+        id: 15,
+        title: "GYM APP ****",
+        img: gymapp,
+        subtitles: ['React', 'Firebase', 'Firebase Database', 'Firebase Auth'],
+        text:
+            <div>
+                Using firebase, which I continue to learn, I developed this application,
+                at the same time I tried to focus on the use and features of firebase rather than design.
+                This application, which consists of 3 pages for now, has a structure that is open to development
+                both in terms of design and in terms of features.<br /><br />
+                The application is basically an agenda where you enter your information and save the sport you do.
+                It calculates the calories you burn using your information and stores this information so that you can access
+                it later. You can see the status of the end users on the main page and there is a small blog section.<br /><br />
+                Features such as diversifying user information, tracking meals and adding weekly sports program can be added
+                to the application. In addition, in the future, I aim to develop it in terms of design.
+            </div>,
+        date: "08/03/2023",
+        link: "https://kfl64t.csb.app/",
+        no: 1,
+    },
+    {
         id: 14,
         title: "Firebase Note App *****",
         img: FirebaseNotes,
-        subtitles: ['React','Firebase','Firebase Database','Firebase Auth'],
+        subtitles: ['React', 'Firebase', 'Firebase Database', 'Firebase Auth'],
         text: <div>In this work, I used Firebase for the first time and included
-             both the Authentication feature and the Database feature in my 
-             project. <br /><br /> Users can create in-app notes by logging into the app 
-             through their google accounts. These notes are stored in the 
-             database and are taken from there and reflected on the screen 
-             to be seen by everyone. Some bugs and design issues for the 
-             application are among the problems that can be resolved in 
-             the future. But for now the app is available.
+            both the Authentication feature and the Database feature in my
+            project. <br /><br /> Users can create in-app notes by logging into the app
+            through their google accounts. These notes are stored in the
+            database and are taken from there and reflected on the screen
+            to be seen by everyone. Some bugs and design issues for the
+            application are among the problems that can be resolved in
+            the future. But for now the app is available.
         </div>,
         date: "28/02/2023",
         link: "https://jm4h9o.csb.app/",
@@ -40,7 +62,7 @@ const projects = [
         id: 13,
         title: "Planti Dashboard *****",
         img: Planti,
-        subtitles: ['React'],
+        subtitles: ['React', 'Design Based Project'],
         text: <div>This is a design-oriented responsive dashboard work.
             In this work, I tried to implement a design that I saw on the internet.
             This project, which I created using React, took about 1 day.
